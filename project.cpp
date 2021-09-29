b5e77b5b08f9dc8ed03c53d2569899962f0d7991
darienMoltaji <d.moltaji@gmail.com>
2021-09-29 13:24:53 -0700

    Update project.cpp

---
+++ project.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a;
+
+cout<<"Hello World";
+cout<<"This is me making another commit, what do you think?";
+cin>> a;
+cout<<"Cool.";
+return 0;
+}
