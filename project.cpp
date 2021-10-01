b1a597ff922ef23f4bf06a0cb4a40f572f9e91d1
darienMoltaji <d.moltaji@gmail.com>
2021-10-01 14:42:39 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,30 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
 
-double dueCost, dueInterest, minPayment, balance;
-char Continue;
-
-do{ 
- 	 cout<<"Enter balance due: ";
-	 cin>>balance;
-	 
-	 if(balance <= 1000){
-	  dueInterest = (balance * 0.015);
-	 }else if (balance > 1000){
-	dueInterest = ((balance - 1000) * 0.01) + (1000) * 0.015;}
-	
-	dueCost = balance + dueInterest;
-
-    minPayment = dueCost<=10? 
-    dueCost : ((dueCost*.1)> 10? (dueCost*.1):10);
-
-	cout<<"Interest on balance:"<< " " << dueCost << endl ;
-	
-	cout<<"Would you like to continue? Press y for yes, n for no \n";
-	cin>> Continue;
-	 
-	}while(Continue =='y' || Continue =='Y');
-	
-return 0;
+int main()
+{
+   int i=0, j=0, count=0;
+   for(i=3 ; i<=100 ; i++){
+       count=0;
+       for(j=2 ; j<=(i/2)+1 ; j++)
+       {
+           if(i%j == 0){
+               ++count;
+               break;
+           }
+       }
+       if(count==0)
+           cout<<i<<",";
+   }
+   cout<<endl;
 }
