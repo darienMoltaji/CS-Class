6cf0222939366e556b90cde0d54fd3eab388da96
darienMoltaji <d.moltaji@gmail.com>
2021-09-30 19:36:17 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -9,3 +9,16 @@ cout<<"Hello World";
 cout<<"This is me making another commit, what do you think?";
 return 0;
 }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float balance = 15000;
+    int dunks = 5;
+    bool light = 1;
+    char d = 'd';
+    string highschool_name = "Laguna Hills High school";
+return 0;
+}
