7ee3b5df3f3e9e99263fca8f72c9f001cfb57fc2
darienMoltaji <d.moltaji@gmail.com>
2021-10-03 15:21:43 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,20 +1,91 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-   int i=0, j=0, count=0;
-   for(i=3 ; i<=100 ; i++){
-       count=0;
-       for(j=2 ; j<=(i/2)+1 ; j++)
-       {
-           if(i%j == 0){
-               ++count;
-               break;
-           }
-       }
-       if(count==0)
-           cout<<i<<",";
-   }
-   cout<<endl;
+int main(){
+
+int day, time_of_day;
+char user_continue;
+enum week{Sun, Mon, Tue, Wed, Thu, Fri, Sat};
+week now = Sun ;
+
+
+cout<< "What is the day of the week?"
+    << " 0 for Sun, 1 for Mon, 2 for Tue, 3 for Wed, 4 for Thu, 5 for Fri, and 6 for Sat. ";
+cin>> day;
+cout<< "What time of the day is it?"<< " (Enter in military time)";
+cin>> time_of_day;
+
+cout<< "\nContinue? (y for yes, n for no)";
+cin>> user_continue;
+
+while (user_continue = 'y'){    
+
+        switch(now){
+            
+            case Sun:
+            if (time_of_day = 800){
+                cout<< "Walk the dog.";
+            }else if (time_of_day = 1200){
+                cout<< "Water the lawn.";
+            }else if (time_of_day = 1500){
+                cout<< "Get groceries";}
+            break;
+            
+            case Mon:
+                if (time_of_day = 800){
+                cout<< "Dust the TV.";
+            }else if (time_of_day = 1200){
+                cout<< "Wash the dishes.";
+            }else if (time_of_day = 1500){
+                cout<< "Take out the trash.";}
+            break;
+            
+            case Tue:
+                    if (time_of_day = 800){
+                cout<< "Make coffee.";
+            }else if (time_of_day = 1200){
+                cout<< "Make lunch. ";
+            }else if (time_of_day = 1500){
+                cout<< "Mop the floor.";}
+            break;
+            
+            case Wed:
+                    if (time_of_day = 800){
+                cout<< "Do homework.";
+            }else if (time_of_day = 1200){
+                cout<< "Clean the toilet.";
+            }else if (time_of_day = 1500){
+                cout<< "Wash the dog.";}
+            break;
+            
+            case Thu:
+                    if (time_of_day = 800){
+                cout<< "Go for a jog.";
+            }else if (time_of_day = 1200){
+                cout<< "Vacuum.";
+            }else if (time_of_day = 1500){
+                cout<< "Mow the lawn.";}
+            break;
+            
+            case Fri:
+                    if (time_of_day = 800){
+                cout<< "Wash windows.";
+            }else if (time_of_day = 1200){
+                cout<< "Do laundry.";
+            }else if (time_of_day = 1500){
+                cout<< "Make up bed.";}
+            break;
+            
+            case Sat:
+                    if (time_of_day = 800){
+                cout<< "Call grandma.";
+            }else if (time_of_day = 1200){
+                cout<< "Take a nap.";
+            }else if (time_of_day = 1500){
+                cout<< "Make a list.";}
+            break;
+        }
+    }
+
+    return 0;
 }
