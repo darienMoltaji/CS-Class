6c3f0d57589dcf4f28bd3e6617a84510bff71461
darienMoltaji <d.moltaji@gmail.com>
2021-10-05 21:39:42 -0700

    Create project2.cpp

---
+++ project2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number, thousands, tens, hundredsm ones;
+    cout<< "Enter a four digit number";
+    cin>> number;
+    ones = number %10;
+
+
+
+    return 0;
+}
