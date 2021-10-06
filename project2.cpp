618c8167a1065f3d000f00b50c8288323bc8e847
darienMoltaji <d.moltaji@gmail.com>
2021-10-05 21:44:38 -0700

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 int main()
 {
+getRoman(int thousands, int hundreds, int tens, int ones){
+    int getRoman;
+    getRoman = thousands+hundreds+tens+ones;
+    return getRoman;
+}
+
     int number, thousands, tens, hundredsm ones;
     cout<< "Enter a four digit number";
     cin>> number;
     ones = number %10;
+    number = number / 10;
+    tens = number %10;
+    number = number / 10;
+    hundreds = number %10;
+    number = number / 10;
+    thousands = number %10;
+    
+    getRoman(thousands, 'M', NULL, NULL);
+    getRoman (hund)
 
 
 
