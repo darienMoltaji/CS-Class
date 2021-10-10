ce758fb0bbaedc77f7044e51a715e2de20787cc1
darienMoltaji <d.moltaji@gmail.com>
2021-10-09 17:17:27 -0700

    Create project3.cpp

---
+++ project3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+double power (double num, double expo){
+    
+    
+    cout << "Enter a number, and the power that you're trying to find";
+    cin >> num;
+    cout << " ";
+    cin >> expo;
+    
+    double result = num * num
+    
+    return num ;
+}
+
+
+
+
+int main()
+{
+    cout << cube(5);
+    
+    
+    return 0;
+}
