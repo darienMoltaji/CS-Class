7610725cebafbb0c503256c5289bd567c56d3d63
darienMoltaji <d.moltaji@gmail.com>
2021-10-11 10:56:38 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+double addition (double x, double y){
+    double z;
+    return z = x + y;
+}
+
 int main(){
 
 
 cout << "Hello"<<"World";
+cout << addition(2,3);
     return 0;
 }
