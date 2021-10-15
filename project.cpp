bb75424e57688e39ea23aed7ac9b07dadbd8b387
darienMoltaji <d.moltaji@gmail.com>
2021-10-15 14:42:34 -0700

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,15 +1,34 @@
-#include <iostream>
-using namespace std;
+#include <iostream>3
+using namespace std; 
 
-double addition (double x, double y){
-    double z;
-    return z = x + y;
+void swapValues (int& userVal1, int& userVal2) {
+   int temp; 
+   temp = userVal1;
+   userVal1 = userVal2; 
+   userVal2 = temp;
+}
+
+void sort(int& userVal1, int& userVal2, int& userVal3){
+    if(userVal1<userVal2){
+        swapValues(userVal1,userVal2);
+    }
+    if(userVal1<userVal3){
+        swapValues(userVal1,userVal3);
+    }
+    if(userVal2<userVal3){
+        swapValues(userVal2,userVal3);
+    }
 }
 
 int main(){
-
-
-cout << "Hello"<<"World";
-cout << addition(2,3);
-    return 0;
+   int userVal1, userVal2, userVal3;
+   cout<<"Enter first number: ";
+   cin>>userVal1;
+   cout<<"Enter second number: ";
+   cin>>userVal2;
+   cout<<"Enter third number: ";
+   cin>>userVal3;
+   sort(userVal1, userVal2, userVal3);
+   cout<<userVal1<<" "<<userVal2<<" "<<userVal3<<endl;
+   return 0;
 }
